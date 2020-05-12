bd554595996ab95ea784baa46492c173ffd87465
Alex Rapala <ajrapala@gmail.com>
2020-05-12 17:48:19 -0500

    up to github

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Commander.Models;
+using Commander.Data;
+using AutoMapper;
+using Commander.Dtos;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace Commander.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+
+        private readonly ICommanderRepo _repository;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommanderRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        // GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
+        {
+            IEnumerable<Command> commandItems = _repository.GetAllCommands();
+
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
+        }
+
+        // GET api/commands/{id}
+        // nesting might be a problem with this in the future?
+        [HttpGet("{id}", Name="GetCommandById")]
+        public ActionResult<CommandReadDto> GetCommandById(int id)
+        {
+            Command commandItem = _repository.GetCommandById(id);
+            if(commandItem != null)
+            {
+                return Ok(_mapper.Map<CommandReadDto>(commandItem));
+            }
+            return NotFound();
+        }
+
+        // POST api/commands
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommand(CommandCreateDto commandCreateDto)
+        {
+            Command commandModel = _mapper.Map<Command>(commandCreateDto);
+
+            _repository.CreateCommand(commandModel);
+            _repository.SaveChanges();
+
+            CommandReadDto commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
+
+            return CreatedAtRoute(nameof(GetCommandById), new { Id = commandReadDto.Id }, commandReadDto);
+        }
+
+        // PUT api/commands/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateCommand(int id, CommandUpdateDto commandUpdateDto)
+        {
+            Command command = _repository.GetCommandById(id);
+
+            if(command == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(commandUpdateDto, command);
+
+            _repository.UpdateCommand(command);
+
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+
+        // PATCH api/commands/{id}
+        [HttpPatch("{id}")]
+        public ActionResult PartialUpdateCommand(int id, JsonPatchDocument<CommandUpdateDto> patchDoc)
+        {
+            Command command = _repository.GetCommandById(id);
+
+            if (command == null)
+            {
+                return NotFound();
+            }
+
+            CommandUpdateDto commandToPatch = _mapper.Map<CommandUpdateDto>(command);
+
+            patchDoc.ApplyTo(commandToPatch, ModelState);
+
+            if(!TryValidateModel(commandToPatch))
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            _mapper.Map(commandToPatch, command);
+
+            _repository.UpdateCommand(command);
+
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+
+
+        // DELETE api/commands/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCommand(int id)
+        {
+            Command command = _repository.GetCommandById(id);
+
+            if (command == null)
+            {
+                return NotFound();
+            }
+
+            _repository.DeleteCommand(command);
+
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Commander.Data;
+using Commander.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+
+namespace Commander
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CommanderContext>(opt => opt.UseNpgsql(
+                Configuration.GetConnectionString("CoommanderConnection")
+            ));
+
+            services.AddControllers().AddNewtonsoftJson(s =>
+            {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
+
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            var swaggerOptions = new SwaggerOptions();
+
+            Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
+            app.UseSwagger(option =>
+            {
+                option.RouteTemplate = swaggerOptions.JsonRoute;
+            });
+
+            app.UseSwaggerUI(option =>
+            {
+                option.SwaggerEndpoint(swaggerOptions.UiEndpoint, swaggerOptions.Description);
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                Console.Write(endpoints);
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
